058102a61d8bdb8025e29d2de1ffdd8706dec520
huangtingjun <htj@addcn.com>
2017-11-06 18:26:18 +0800

    學習基礎

--- src/components/page1.vue
+++ src/components/page1.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div class="sew" v-bind:title="ss">asda</div>
+    <p v-if="go">aaaa</p>
+    <div class="list" v-for="v in list" v-bind:key="v.id">
+      {{v.a}}
+    </div>
+    <span v-on:click="showall">show</span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Page1',
+  methods:{
+    showall:function(){
+      console.log(this)
+    }
+  },
   data () {
     return {
-      msg: 'Welcome to page1'
+      msg: 'Welcome to page1',
+      ss: new Date().toLocaleString(),
+      go: 1,
+      list: [
+        {a:111},
+        {a:223},
+        {a:44}
+      ]
     }
   }
 }
